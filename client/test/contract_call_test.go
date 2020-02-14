487136304acbb5adb873acd77e862708da99a8bd
iavl <z6521256@gmail.com>
2020-02-14 20:35:53 +0800

    use abi to construct payload

--- client/test/contract_call_test.go
+++ client/test/contract_call_test.go
@@ -2,9 +2,13 @@ package test
 
 import (
 	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 	"strconv"
+	"strings"
 	"testing"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
 
 	"github.com/netcloth/go-sdk/client"
 	"github.com/netcloth/go-sdk/util"
@@ -15,29 +19,29 @@ import (
 
 const (
 	// contract args
+	functionName   = "recall"
 	fromBech32Addr = "nch13f5tmt88z5lkx8p45hv7a327nc0tpjzlwsq35e"
 	toBech32Addr   = "nch1zypvh2q606ztw4elfgla0p6x4eruz3md6euv2t"
-	pubKey         = "1100000000000000000000000000000000000000000000000000000000000001"
+	pubKey         = 1025
 	timestamp      = 1581065043
-	r              = "1100000000000000000000000000000000000000000000000000000000000001"
-	s              = "1100000000000000000000000000000000000000000000000000000000000001"
+	r              = 2049
+	s              = 4097
 
 	// contract address
-	contractBech32Addr = "nch1vdm60zm5jr5yn3aj0dkfkghf23x4t2yw9y2j4f"
-
+	contractBech32Addr = "nch1kuzlpyesw7vuh65dlpdvy5j5q5hmhygvw73f2p"
+	contractAbi        = `[{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"from","type":"address"},{"indexed":false,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"timestamp","type":"uint256"},{"indexed":false,"internalType":"int64","name":"pk","type":"int64"}],"name":"Recall","type":"event"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"timestamp","type":"uint256"},{"internalType":"int64","name":"r","type":"int64"},{"internalType":"int64","name":"s","type":"int64"}],"name":"ecrecoverDecode","outputs":[{"internalType":"address","name":"addr","type":"address"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"address","name":"from","type":"address"}],"name":"queryParams","outputs":[{"internalType":"int64","name":"pubkey","type":"int64"},{"internalType":"uint256","name":"timestamp","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"timestamp","type":"uint256"},{"internalType":"int64","name":"pk","type":"int64"},{"internalType":"int64","name":"r","type":"int64"},{"internalType":"int64","name":"s","type":"int64"}],"name":"recall","outputs":[],"stateMutability":"nonpayable","type":"function"}]`
 	/*
 		    function recall(address from, address to, uint timestamp, bytes32 pubkey, byte t, bytes32 r, bytes32 s, byte v) public {
 			from 和to 是地址的二进制，如果是bech32地址需要先转为二进制的地址即[20]byte类型，再按照二进制的字符串形式打印成40个字符的字符串
 			timestamp是时间戳，需要填充为32字节
 			pubkey为公钥，需要填充为32字节
-			pubKeyType为公钥类型，1表示个人，2表示群,需要填充为32字节
-			最后3个%s是签名的r，s的二进制字符串
+			r，s为签名
 	*/
 
 	addressPadZeros = "000000000000000000000000"
 
 	// fc6a54a8 is recall function signature
-	payloadTemplate = "fc6a54a8%s%s%064x%s%s%s"
+	payloadTemplate = "%s%s%064x%064x%064x%064x"
 )
 
 var (
@@ -56,15 +60,21 @@ func Test_ContractCall(t *testing.T) {
 
 	toAddrBin, err := sdk.AccAddressFromBech32(toBech32Addr)
 	require.True(t, err == nil)
-	toAddrStr := hexutil.Encode(toAddrBin)
-	//fmt.Println(fmt.Sprintf("%x", toAddrBin.Bytes()))
-	//fmt.Println(toAddrStr)
+	toAddrStr := hexutil.Encode(toAddrBin.Bytes())
 
 	payloadStr := fmt.Sprintf(payloadTemplate, addressPadZeros+fromAddrStr, addressPadZeros+toAddrStr, timestamp, pubKey, r, s)
+	//fmt.Println(fmt.Sprintf("payload:         %s ", payloadStr))
+	argsBinary, err := hex.DecodeString(payloadStr)
 
-	t.Log(payloadStr)
-	payload, err := hexutil.Decode(payloadStr)
-	require.True(t, err == nil)
+	var payload []byte
+	abiObj, _ := abi.JSON(strings.NewReader(contractAbi))
+	m, _ := abiObj.Methods[functionName]
+
+	readyArgs, err := m.Inputs.UnpackValues(argsBinary)
+	require.NoError(t, err)
+
+	payload, err = abiObj.Pack(functionName, readyArgs...)
+	require.NoError(t, err)
 
 	res, err := client.ContractCall(contractBech32Addr, payload, amount, true)
 	if err != nil {
@@ -74,7 +84,7 @@ func Test_ContractCall(t *testing.T) {
 	}
 }
 
-const txHash = "EA5D94E02EF77AF61D3CEA6056348D1E400C0728E059EB57F3D8A3E896390427"
+const txHash = "E96A859D825377A8BD97C76BF77B39C2D5078F61F375EC8F6B31C90E55FC8FD0"
 
 func Test_ContractQuery(t *testing.T) {
 	client, err := client.NewNCHClient(yaml_path)
@@ -86,40 +96,40 @@ func Test_ContractQuery(t *testing.T) {
 
 	t.Log(r.Result.Logs[0].Data)
 
-	/*
-		item := r.Result.Logs[0].Data
-		s, _ := base64.StdEncoding.DecodeString(item)
-		fmt.Println(fmt.Sprintf("%d, %x", len(s), s))
+	item := r.Result.Logs[0].Data
+	s, _ := base64.StdEncoding.DecodeString(item)
+	fmt.Println(fmt.Sprintf("%d, %x", len(s), s))
 
-		// 第一个byte32为from地址
-		a := fmt.Sprintf("%x", s[12:32])
-		// 第二个byte32为to地址
-		b := fmt.Sprintf("%x", s[44:64])
-		// 为int64类型的timestame
-		c := fmt.Sprintf("%x", s[64:96])
+	// 第一个byte32为from地址
+	a := fmt.Sprintf("%x", s[12:32])
+	// 第二个byte32为to地址
+	b := fmt.Sprintf("%x", s[44:64])
+	// 为int64类型的timestame
+	c := fmt.Sprintf("%x", s[64:96])
 
-		d := fmt.Sprintf("%x", s[96:128])
+	// address - from
+	accA, _ := sdk.AccAddressFromHex(a)
+	fmt.Println(fmt.Sprintf("%s --> %s", a, accA.String()))
 
-		//e ;= fmt.Sprintf("%x", s[128:])
+	// address - to
+	accB, _ := sdk.AccAddressFromHex(b)
+	fmt.Println(fmt.Sprintf("%s --> %s", b, accB.String()))
 
-		// 输出
-		accA, _ := sdk.AccAddressFromHex(a)
-		fmt.Println(fmt.Sprintf("%s --> %s", a, accA.String()))
+	// uint - timestamp
+	timestamp, _ := strconv.ParseUint(c, 16, 64)
+	fmt.Println(fmt.Sprintf("%s --> %d", c, timestamp))
 
-		accB, _ := sdk.AccAddressFromHex(b)
-		fmt.Println(fmt.Sprintf("%s --> %s", b, accB.String()))
+	// int64 - public key
+	d := fmt.Sprintf("%x", s[96:128])
+	pk, _ := strconv.ParseUint(d, 16, 64)
+	fmt.Println(fmt.Sprintf("%s --> %d", d, pk))
 
-		timestamp, _ := strconv.ParseUint(c, 16, 64)
-		fmt.Println(fmt.Sprintf("%s --> %d", c, timestamp))
-
-		fmt.Println(fmt.Sprintf("%s --> %s", d, d))
-	*/
 }
 
 func Test_QueryContractEvents(t *testing.T) {
 	// 遍历 [start, end] 之间的区块
-	startBlockNum := int64(1287)
-	endBlockNum := int64(1289)
+	startBlockNum := int64(6289)
+	endBlockNum := int64(6290)
 
 	client, err := client.NewNCHClient(yaml_path)
 	require.True(t, err == nil)
@@ -140,20 +150,23 @@ func Test_QueryContractEvents(t *testing.T) {
 		b := fmt.Sprintf("%x", s[44:64])
 		// 为int64类型的timestame
 		c := fmt.Sprintf("%x", s[64:96])
-		// pubkey
-		d := fmt.Sprintf("%x", s[96:128])
 
-		// 输出
+		// address - from
 		accA, _ := sdk.AccAddressFromHex(a)
 		fmt.Println(fmt.Sprintf("%s --> %s", a, accA.String()))
 
+		// address - to
 		accB, _ := sdk.AccAddressFromHex(b)
 		fmt.Println(fmt.Sprintf("%s --> %s", b, accB.String()))
 
+		// uint - timestamp
 		timestamp, _ := strconv.ParseUint(c, 16, 64)
 		fmt.Println(fmt.Sprintf("%s --> %d", c, timestamp))
 
-		fmt.Println(fmt.Sprintf("%s --> %s", d, d))
+		// int64 - public key
+		d := fmt.Sprintf("%x", s[96:128])
+		pk, _ := strconv.ParseUint(d, 16, 64)
+		fmt.Println(fmt.Sprintf("%s --> %d", d, pk))
 
 		t.Log(item)
 	}
